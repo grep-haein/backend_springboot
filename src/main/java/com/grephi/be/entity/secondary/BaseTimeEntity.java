1531f63cea1278ee3846b61c9e85f3ff7dbafd51
grep.hi <grep.haein@gmail.com>
2024-04-02 23:21:13 +0900

    add datetime control entity

---
+++ src/main/java/com/grephi/be/entity/secondary/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.grephi.be.entity.secondary;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime created_date;
+
+    @LastModifiedDate
+    private LocalDateTime modified_date;
+}
--- src/main/java/com/grephi/be/entity/secondary/HaviRepository.java
+++ src/main/java/com/grephi/be/entity/secondary/HaviRepository.java
@@ -1,6 +1,7 @@
 package com.grephi.be.entity.secondary;
 
 import com.grephi.be.model.SearchCondition;
+import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
